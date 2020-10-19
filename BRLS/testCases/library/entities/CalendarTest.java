7e37e76d6b3c80233c6a827a48c6029f84b5929c
abhimaniac4 <bhat_abhimanyu@yahoo.combhat_abhimanyu@yahoo.combhat_abhimanyu@yahoo.com>
2020-10-19 22:46:39 +1000

    tested getDaysDifference in Calendar

--- BRLS/testCases/library/entities/CalendarTest.java
+++ BRLS/testCases/library/entities/CalendarTest.java
@@ -1,14 +1,34 @@
 package library.entities;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.List;
+import java.util.Date;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import library.entities.ICalendar;
+
+
+@ExtendWith(MockitoExtension.class) 
+
 
 class CalendarTest {
+	
+    @Mock IBook mockBook;
+	
+	@InjectMocks
+
+	Calendar mockCalendar;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -27,8 +47,14 @@ class CalendarTest {
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testGetDaysDifference() {
+		//arrange
+		mockCalendar.getDate();
+		//act
+		mockCalendar.incrementDate(3);
+		Date targetDate = mockCalendar.getDate();
+		//assert
+		assertEquals(3,mockCalendar.getDaysDifference(targetDate));
 	}
 
 }
